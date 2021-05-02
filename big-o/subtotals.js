1ec7555c10976b0324492b3e74ad49ac1947eadd
Dhari <dmakwt@gmail.com>
2021-05-01 22:29:09 -0500

    add more space complexity example

---
+++ big-o/subtotals.js
@@ -0,0 +1,13 @@
+// Space Complexity: O(n)space!
+
+function subtotals(array) {
+    var subtotalArray = Array(array.length);
+    for (var i = 0; i < array.length; i++) {
+        var subtotal = 0;
+        for (var j = 0; j <= i; j++) {
+            subtotal += array[j];
+        }
+        subtotalArray[i] = subtotal;
+    }
+    return subtotalArray;
+}
