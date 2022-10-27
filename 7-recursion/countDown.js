8cdcf3973355cb888bb02d0b07a6639d80cfb785
Dhari <dmakwt@gmail.com>
2022-10-27 22:22:05 +0300

    Add recursion

---
+++ 7-recursion/countDown.js
@@ -0,0 +1,13 @@
+const countDown = (num) => {
+  if (num <= 0) {
+    console.log('All done!');
+    return;
+  }
+
+  console.log(num);
+  num--;
+
+  countDown(num);
+};
+
+countDown(10);
---
+++ 7-recursion/sumRange.js
@@ -0,0 +1,9 @@
+const sumRange = (num) => {
+  if (num === 1) {
+    return 1;
+  }
+
+  return num + sumRange(num - 1);
+};
+
+console.log(sumRange(3));
