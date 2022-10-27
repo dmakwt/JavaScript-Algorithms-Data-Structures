e6622d84e741251fc0907a18fb503708f37f6277
Dhari <dmakwt@gmail.com>
2022-10-28 00:55:11 +0300

    Add factorial example

---
+++ 7-recursion/factorial.js
@@ -0,0 +1,24 @@
+// Non-recursive solution (Iterative solution)
+const factorial = (num) => {
+  let total = 1;
+
+  for (let i = num; i > 1; i--) {
+    total *= i;
+  }
+
+  return total;
+};
+
+console.log(factorial(10));
+
+// Recursive solution
+
+const factorialRe = (num) => {
+  if (num === 1) {
+    return 1;
+  }
+
+  return num * factorialRe(num - 1);
+};
+
+console.log(factorialRe(10));
