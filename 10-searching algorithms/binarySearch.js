b8930ad98da3a20199f77d68f47a5fbfb778ec07
Dhari <dmakwt@gmail.com>
2022-10-28 19:59:10 +0300

    Add binary search

---
+++ 10-searching algorithms/binarySearch.js
@@ -0,0 +1,18 @@
+const binarySearch = (arr, elem) => {
+  var start = 0;
+  var end = arr.length - 1;
+  var middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] !== elem && start <= end) {
+    if (elem < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+
+  return arr[middle] === elem ? middle : -1;
+};
+
+console.log(binarySearch([1, 2, 3, 4, 5], 5));
