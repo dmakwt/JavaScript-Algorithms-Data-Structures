03e89290c2df40fe1d570a203edf044b4613556e
Dhari <dmakwt@gmail.com>
2022-10-30 02:19:41 +0300

    Optimizzed bubbleSort with noSwaps

--- 11-bubble-sort/bubbleSort.js
+++ 11-bubble-sort/bubbleSort.js
@@ -11,13 +11,31 @@
 //   return arr;
 // };
 
+// const bubbleSort = (arr) => {
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//   }
+
+//   return arr;
+// };
+
+// Optimized with noSwaps
 const bubbleSort = (arr) => {
+  let noSwaps;
+
   for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
 
   return arr;
