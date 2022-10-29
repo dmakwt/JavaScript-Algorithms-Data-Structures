116d614b370b636f4eccf538288a0344c9331fa7
Dhari <dmakwt@gmail.com>
2022-10-30 02:11:47 +0300

    Add bubble sort

---
+++ 11-bubble-sort/bubbleSort.js
@@ -0,0 +1,26 @@
+// Naive solution
+// const bubbleSort = (arr) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr.length; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//   }
+
+//   return arr;
+// };
+
+const bubbleSort = (arr) => {
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+};
+
+console.log(bubbleSort([1, 35, 2, 23, 3]));
