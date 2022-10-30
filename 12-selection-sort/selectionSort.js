69f318d4d8ad7cbda70760383dbfff0945896541
Dhari <dmakwt@gmail.com>
2022-10-30 20:16:40 +0300

    Add selection sort

---
+++ 12-selection-sort/selectionSort.js
@@ -0,0 +1,19 @@
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+
+    if (i !== lowest) {
+      [arr[i], arr[lowest]] = [arr[lowest], arr[i]];
+    }
+  }
+
+  return arr;
+};
+
+console.log(selectionSort([1, 35, 2, 23, 3]));
