862dfafa05ae5c0e52ca9638686c31a1a722a6d4
Dhari <dmakwt@gmail.com>
2022-11-05 13:52:48 +0300

    Add singly-linked-lists

---
+++ 19-singly-linked-lists/singly-linked-lists.js
@@ -0,0 +1,66 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
+  }
+
+  //   traverse() {
+  //     let current = this.head;
+  //     while (current) {
+  //       console.log(current.val);
+  //       current = current.next;
+  //     }
+  //   }
+
+  pop() {
+    if (!this.tail) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+
+    return current;
+  }
+}
+
+let element = new SinglyLinkedList();
+
+console.log(element.pop());
+console.log(element.pop());
+
+console.log(element.pop());
+
+console.log(element);
